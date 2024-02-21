d6cde5aece6ab555d2b3fac52db5d4ee09d2876b
Ian Chamberlain <ichamberlain@juniper.net>
2024-02-21 12:23:52 -0500

    More obvious success in --quiet mode

--- python/compile/compile_pip_requirements_wrapper_template.sh
+++ python/compile/compile_pip_requirements_wrapper_template.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -e
+
 if [ -z ${BUILD_WORKSPACE_DIRECTORY+x} ]; then
     echo "This script must be executed with Bazel"
     exit 1
@@ -13,10 +16,13 @@ echo "Compiling $REQUIREMENTS_TXT_PATH"
 
 export CUSTOM_COMPILE_COMMAND="@@CUSTOM_COMPILE_COMMAND@@"
 $PYTHON_INTERPRETER_PATH $PIP_COMPILE_BINARY \
-    --output-file $REQUIREMENTS_TXT_PATH \
+    --output-file "$REQUIREMENTS_TXT_PATH" \
     "@@QUIET_ARG@@" \
+    --header \
     --no-emit-index-url \
     --generate-hashes \
     --allow-unsafe \
     "$@" \
     $REQUIREMENTS_IN_PATH
+
+echo "Compiled $REQUIREMENTS_TXT_PATH successfully!"
